a9c325408a7c951013e8ab79dfcc31f8bb249e83
Pan Yan Ru <q0u04bj3@gmail.com>
2017-06-28 15:32:07 +0800

    Update Fib.py

--- Fib.py
+++ Fib.py
@@ -1,11 +1,14 @@
 import sys
 
-#目前項是前兩項的總和
 def fib():
     a,b = 0,1
     while True:
         a, b = b, a + b
         yield b
 
-for i in range(1,len(sys.argv)):
-    print fib(int(sys.argv[i]))
+sys.argv[1] = fib()
+print sys.argv[1].next()
+print sys.argv[1].next()
+print sys.argv[1].next()
+print sys.argv[1].next()
+print sys.argv[1].next()
