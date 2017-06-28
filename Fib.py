7168d1d6da489fc3619fa00efd2640f97d86c4fd
Pan Yan Ru <q0u04bj3@gmail.com>
2017-06-28 14:53:01 +0800

    Update Fib.py

--- Fib.py
+++ Fib.py
@@ -1,7 +1,11 @@
+import sys
+
+#目前項是前兩項的總和
 def fib():
     a,b = 0,1
     while True:
         a, b = b, a + b
         yield b
 
-#目前項是前兩項的總和
+for i in range(1,len(sys.argv)):
+    print fib(int(sys.argv[i]))
