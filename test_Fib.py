eac61ed5fb432e50f5bcc754fd12a56f34eab720
Pan Yan Ru <q0u04bj3@gmail.com>
2017-06-30 15:51:29 +0800

    Update test_Fib.py

--- test_Fib.py
+++ test_Fib.py
@@ -3,13 +3,29 @@ from Fib import fib
 
 class FibonacciTest(unittest.TestCase):
     
-    def test_fid(self):
+    def test_fid1(self):
         c=fib()
-        self.assertEqual(c.next(), 1)
-        self.assertEqual(c.next(), 2)
-        self.assertEqual(c.next(), 3)
-        self.assertEqual(c.next(), 5)
-        self.assertEqual(c.next(), 8)
+        except_value=1
+        for i in range(1):
+            value = c.next()
+        self.assertEqual(except_value, value)
+
+
+    def test_fid5(self):
+        c=fib()
+        except_value=8
+        for i in range(5):
+            value = c.next()
+        self.assertEqual(except_value, value)
+
+
+    def test_fid20(self):
+        c=fib()
+        except_value=10946
+        for i in range(20):
+            value = c.next()
+        self.assertEqual(except_value, value)
+
 
 if __name__ == '__main__':
     unittest.main()
